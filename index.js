23a634d3b71375067ffa49a516c9d446fae9eba0
Milan Mehra <29466935+milanmdev@users.noreply.github.com>
2020-09-07 03:49:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,16 @@ module.exports = {
         output += letters.substring(random = Math.floor(Math.random() * letters.length), random + 1);
       }
       return output;
+    },
+
+  //Random Colow Generator
+  randomColors: function() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
     }
 
 
