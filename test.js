10d70578b7362b152367ba42c925426970a7b2cc
Milan Mehra <29466935+milanmdev@users.noreply.github.com>
2020-09-07 11:30:28 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
 var client = require("./index.js");
 
+console.log(client.randomNoGenerator(5, 10))
+
+console.log(client.randomLetters(10))
+
+console.log(client.randomColors())
+
 console.log(client.randomNames())
